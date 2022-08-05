953b7bd57d54d67547d32413f87c16d6abb90305
Dan <dan.krajnak@gmail.com>
2022-08-05 16:40:05 -0400

    Add page titles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <DefaultSeo title="Visit Dan" />
       <Component {...pageProps} />
     </RecoilRoot>
   );
